8b8cafe91b37065bfefe85ff13eb3c92d4e2e166
acse-8a646952 <sgzyan25@126.com>
2022-10-18 11:46:20 +0100

    -a

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,5 @@
 
-__all__ = ['my_sum']
+__all__ = ['my_sum', 'factorial']
 
 
 def my_sum(iterable):
@@ -7,3 +7,10 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+
+
+def factorial(n):
+    return n * factorial(n-1) if n-1 > 0 else 1
+
+print(factorial(4))
